2616da391423ef9e978df98fa3298fb400aa93c5
Suresh Kumar <sureshdsk91@gmail.com>
2021-08-15 22:56:19 +0530

    dashboard charts updated

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,19 @@ import { subDays, eachDayOfInterval, format } from 'date-fns';
 export default function Home() {
   const [todayPie, setTodaysMood] = useState(null)
   const [yesterPie, setYestMood] = useState(null)
+  const [weekPie, setWeekPie] = useState(null)
   const [lastWeekData, setLastWeekMood] = useState(null)
-
+ 
   let today = new Date();
     
   useEffect(() => {
     fetchTodaysMood()
     fetchYesterdaysMood()
-    fetchLastWeeksMood()
+    fetchLast7DaysMood()
+    fetchLast30DaysMood()
   }, [])
+
+  
 
   const fetchTodaysMood = async () => {
     let { error, data } = await supabase.from('mood_report').select().filter(
@@ -111,8 +115,68 @@ export default function Home() {
     setYestMood(_yestPie)
   }
 
-  const fetchLastWeeksMood = async () => {
-    const aWeekFromNow = subDays(today, 6);
+  const fetchLast7DaysMood = async () => {
+    let filterDaysNum = 6;
+    const aWeekFromNow = subDays(today, filterDaysNum);
+    const thisWeek = eachDayOfInterval(
+      { start: aWeekFromNow, end: today },
+    );
+    
+    let thisWeekDays =[]
+    for (let d of thisWeek){
+      thisWeekDays.push(format(d, "yyyy-MM-dd"))
+    }
+    
+   
+    let { error, data } = await supabase.from('mood_report').select().in(
+      'created_dt', thisWeekDays
+    ).order('created_dt', {ascending:false})
+    if (error) {
+      console.log(error.message)
+      return
+    }
+    const emptyArray = Array(filterDaysNum+1).fill(0);
+    let WeekPieDataLabel = {sick: 0, sad: 1, happy:2, neutral: 3, cant_tell: 4}
+    let WeekPieData = [0, 0, 0, 0, 0]
+    for (let row of data){
+      // console.log(row)
+      WeekPieData[WeekPieDataLabel[row.mood]] += row.count;
+    }
+    // console.log(todayPieData)
+
+    const _WeekPie = {
+      labels: ['Sick', 'Sad', 'Happy', 'Neutral', 'Cant'],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: WeekPieData,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            
+          ],
+          borderWidth: 1,
+        },
+      ],
+    }; 
+
+    setWeekPie(_WeekPie)
+  }
+
+  const fetchLast30DaysMood = async () => {
+    let filterDaysNum = 29;
+    const aWeekFromNow = subDays(today, filterDaysNum);
     const thisWeek = eachDayOfInterval(
       { start: aWeekFromNow, end: today },
     );
@@ -131,60 +195,79 @@ export default function Home() {
       console.log(error.message)
       return
     }
-
-    let dataWeek = {sick: [0, 0, 0, 0, 0, 0, 0], 
-      sad: [0, 0, 0, 0, 0, 0, 0], 
-      happy:[0, 0, 0, 0, 0, 0, 0], 
-      neutral: [0, 0, 0, 0, 0, 0, 0],
-       cant_tell: [0, 0, 0, 0, 0, 0, 0]}
+    const emptyArray = Array(filterDaysNum+1).fill(0);
+    const dataWeek = {
+      sick: [...emptyArray], 
+      sad: [...emptyArray], 
+      happy: [...emptyArray], 
+      neutral: [...emptyArray],
+      cant_tell: [...emptyArray]
+    }
     
     for (let row of data){
       try {
-        console.log(row, thisWeekDays.indexOf(row.created_dt))
-        dataWeek[row.mood][thisWeekDays.indexOf(row.created_dt)] += row.count;
+        
+        console.log("***********")
+        console.log(row, thisWeekDays, thisWeekDays.indexOf(row.created_dt))
+        console.log(dataWeek[row.mood])
+        console.log(dataWeek[row.mood][thisWeekDays.indexOf(row.created_dt)])
+        dataWeek[row.mood][thisWeekDays.indexOf(row.created_dt)] = dataWeek[row.mood][thisWeekDays.indexOf(row.created_dt)] + row.count;
+        console.log(dataWeek[row.mood][thisWeekDays.indexOf(row.created_dt)])
+        console.table(dataWeek)
       } catch (error) {
         console.error(error)
         console.error(row)
       }
       
     }
-    console.table(dataWeek)
+    
 
     const _lastWeekData = {
       labels: [...thisWeekDays],
       datasets: [
         {
           label: '# of Sick',
+          type: 'line',
           data: dataWeek.sick,
-          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
         },
         {
           label: '# of Sad',
+          type: 'line',
           data: dataWeek.sad,
-          backgroundColor: 'rgb(54, 162, 235)',
+          borderColor: 'rgb(54, 162, 235)',
         },
         {
           label: '# of Happy',
+          type: 'line',
           data: dataWeek.happy,
-          backgroundColor: 'rgb(75, 192, 192)',
+          borderColor: 'rgb(75, 192, 192)',
         },
         {
           label: '# of Neutral',
+          type: 'line',
           data: dataWeek.neutral,
-          backgroundColor: 'rgb(45, 45, 80)',
+          borderColor: 'rgb(45, 45, 80)',
         },
         {
           label: '# of Cant',
+          type: 'line',
           data: dataWeek.cant_tell,
-          backgroundColor: 'rgb(80, 50, 192)',
+          borderColor: 'rgb(80, 50, 192)', //backgroundColor
         },
+        // {
+        //   type: 'line',
+        //   label: 'Happy',
+        //   borderColor: '#000',
+        //   borderWidth: 2,
+        //   fill: false,
+        //   data: dataWeek.happy
+        // },
       ],
     };
 
     setLastWeekMood(_lastWeekData)
   }
-
-  
 
   
   
@@ -209,6 +292,15 @@ export default function Home() {
           <h2>Culture Sheperd's Dashboard</h2>
           
           <div className="flex flex-wrap" >
+
+            <div className="flex-column" style={{width:'33.33%'}}>
+                <div class="card fixed block" style={{height:'96%'}}>
+                <h2>Last 7 Day's Team Mood</h2>
+                  <div style={{width:'auto'}}>
+                        <Pie data={weekPie} />
+                  </div>
+                </div>
+            </div>
             <div className="flex-column" style={{width:'33.33%'}}>
                 <div class="card fixed block">
                     <h2>Yesterday's Team Mood</h2>
@@ -227,19 +319,15 @@ export default function Home() {
                   </div>
                 </div>
             </div>
-            <div className="flex-column" style={{width:'33.33%'}}>
-                <div class="card fixed block" style={{height:'96%'}}>
-                <h2>Last week Insights</h2>
-                  <p>Last week 2 members were sad</p>
-                  <p>Last week 10 members were happy</p>
-                  <p>Last week 2 members were sick</p>
-                  <p>Last week 2 members does not want to share their feeling</p>
-                </div>
-            </div>
+           
 
             <div className="flex-column" style={{width:'100%'}}>
                 <div class="card fixed block">
-                <h2>Last Week's Mood</h2>
+                <h2>Last 30 Day's Mood</h2> 
+                {/* <select onChange={(event)=> { changeFilterOption(event.target.value) }} value={filterOption}>
+                  <option value="Last_7_Days">Last 7 Days</option>
+                  <option value="Last_30_Days">Last 30 Days</option>
+                </select> */}
                 <Bar data={lastWeekData} options={lastWeekBarOptions} />
                 </div>
             </div>
